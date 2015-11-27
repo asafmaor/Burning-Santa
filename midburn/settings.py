c5fca6347e812de0ed669f5f8f497ef1df36ea33
Asaf Maor <asafmaor1986@gmail.com>
2015-11-27 14:49:42 +0200

    bla

--- midburn/settings.py
+++ midburn/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles'
-    'midburn.midburn.'
+    # 'midburn.midburn.santa'
 )
 
 MIDDLEWARE_CLASSES = (
