036280ce2de67c1e39ec9e8c0c99eb3be0d6dbf2
Asaf Maor <asafmaor1986@gmail.com>
2015-11-27 14:44:37 +0200

    fixed shit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Midburn-Santa
+Create your own working dir
+Windows
+c:\midburn
+using command line
+git clone https://github.com/asafmaor/Burning-Santa.git c:\midburn\burning-santa
--- midburn/settings.py
+++ midburn/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
+    'midburn.midburn.'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -74,11 +75,15 @@ WSGI_APPLICATION = 'midburn.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.mysql', 'NAME': 'midburn',
+        'USER': 'root', 'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
+    },
 }
 
 
